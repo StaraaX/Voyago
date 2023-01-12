210eb7030b7e93f305d0f5631966b3c034e32c66
StaraaX <100290028+StaraaX@users.noreply.github.com>
2023-01-12 12:55:28 +0100

    CMS

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,16 @@ import Header_bottom from "../components/Header_bottom";
 import Features from '../components/Features.jsx';
 import Content from '../components/Content.jsx';
 import Testimonials from "../components/Testimonials.jsx";
+import Head from "next/head"
 
 
 export default function Home() {
   return (
 
     <div>
+      <Head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
       <Header_bottom />
       <Features />
       <Content />
